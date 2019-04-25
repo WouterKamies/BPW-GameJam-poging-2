82f4efe334a5f9518b80a6a4f18654469c97ab85
daniel <9087597+daankrs@users.noreply.github.com>
2019-04-25 11:54:27 +0200

    Fixed Leap Driver

---
+++ BPW GameJam/Assets/ScriptScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptScript : MonoBehaviour
+{
+    public bool enter = true;
+    public bool stay = true;
+    public bool exit = true;
+    public float moveSpeed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (enter)
+        {
+            Debug.Log("entered");
+        }
+    }
+
+    // stayCount allows the OnTriggerStay to be displayed less often
+    // than it actually occurs.
+    private float stayCount = 0.0f;
+    private void OnTriggerStay(Collider other)
+    {
+        if (stay)
+        {
+            if (stayCount > 0.25f)
+            {
+                Debug.Log("staying");
+                stayCount = stayCount - 0.25f;
+            }
+            else
+            {
+                stayCount = stayCount + Time.deltaTime;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (exit)
+        {
+            Debug.Log("exit");
+        }
+    }
+}
