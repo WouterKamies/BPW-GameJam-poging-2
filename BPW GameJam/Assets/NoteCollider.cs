ab6d06d4e2dad8df8e2f582aa08606d1b5721a59
ArjoNagelhout <arjo.nagelhout@gmail.com>
2019-04-25 11:25:34 +0200

    Oepsie hopelijk is het niet weg

---
+++ BPW GameJam/Assets/NoteCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteCollider : MonoBehaviour
+{
+    public string displayString;
+    public int score;
+
+    public bool isColliding;
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Note")
+        {
+        }
+    }
+}
---
+++ BPW GameJam/Assets/NoteDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteDestroyer : MonoBehaviour
+{
+
+    public GameController gameController;
+    public int score;
+    public string displayString;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Note")
+        {
+            Destroy(collision.gameObject);
+            gameController.score += score;
+            gameController.ShowText(displayString);
+        }
+    }
+}
